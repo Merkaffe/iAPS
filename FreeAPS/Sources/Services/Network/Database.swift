918da722320f97df28a24c28c68160a667f1edd6
Jon Mårtensson <jon.m@live.se>
2024-07-01 00:21:53 +0200

    not done

--- FreeAPS/Sources/Services/Network/Database.swift
+++ FreeAPS/Sources/Services/Network/Database.swift
@@ -21,11 +21,10 @@ class Database {
 
 extension Database {
     func fetchPreferences() -> AnyPublisher<Preferences, Swift.Error> {
-        let statURL = IAPSconfig.statURL
         var components = URLComponents()
-        components.scheme = statURL.scheme
-        components.host = statURL.host
-        components.port = statURL.port
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
         components.path = "/download.php?token=" + token + "&section=preferences"
 
         var request = URLRequest(url: components.url!)
@@ -43,11 +42,10 @@ extension Database {
     }
 
     func fetchSettings() -> AnyPublisher<FreeAPSSettings, Swift.Error> {
-        let statURL = IAPSconfig.statURL
         var components = URLComponents()
-        components.scheme = statURL.scheme
-        components.host = statURL.host
-        components.port = statURL.port
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
         components.path = "/download.php?token=" + token + "&section=preferences"
 
         var request = URLRequest(url: components.url!)
@@ -61,11 +59,10 @@ extension Database {
     }
 
     func fetchProfile() -> AnyPublisher<DatabaseProfileStore, Swift.Error> {
-        let statURL = IAPSconfig.statURL
         var components = URLComponents()
-        components.scheme = statURL.scheme
-        components.host = statURL.host
-        components.port = statURL.port
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
         components.path = "/download.php?token=" + token + "&section=profile"
 
         var request = URLRequest(url: components.url!)
@@ -75,6 +72,110 @@ extension Database {
         return service.run(request)
             .retry(Config.retryCount)
             .decode(type: DatabaseProfileStore.self, decoder: JSONCoding.decoder)
+            .eraseToAnyPublisher()
+    }
+
+    func uploadSettingsToDatabase(_ profile: NightscoutProfileStore) -> AnyPublisher<Void, Swift.Error> {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = Config.sharePath
+
+        var request = URLRequest(url: components.url!)
+        request.allowsConstrainedNetworkAccess = false
+        request.timeoutInterval = Config.timeout
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        request.httpBody = try! JSONCoding.encoder.encode(profile)
+        request.httpMethod = "POST"
+
+        return service.run(request)
+            .retry(Config.retryCount)
+            .map { _ in () }
+            .eraseToAnyPublisher()
+    }
+
+    func uploadStats(_ stats: NightscoutStatistics) -> AnyPublisher<Void, Swift.Error> {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = Config.sharePath
+
+        var request = URLRequest(url: components.url!)
+        request.allowsConstrainedNetworkAccess = false
+        request.timeoutInterval = Config.timeout
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try! JSONCoding.encoder.encode(stats)
+        request.httpMethod = "POST"
+
+        return service.run(request)
+            .retry(Config.retryCount)
+            .map { _ in () }
+            .eraseToAnyPublisher()
+    }
+
+    func fetchVersion() -> AnyPublisher<Version, Swift.Error> {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = Config.versionPath
+
+        var request = URLRequest(url: components.url!)
+        request.allowsConstrainedNetworkAccess = true
+        request.timeoutInterval = Config.timeout
+
+        return service.run(request)
+            .retry(Config.retryCount)
+            .decode(type: Version.self, decoder: JSONCoding.decoder)
+            .catch { error -> AnyPublisher<Version, Swift.Error> in
+                warning(.nightscout, "Version fetching error: \(error.localizedDescription) \(request)")
+                return Just(Version(main: "", dev: "")).setFailureType(to: Swift.Error.self).eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func uploadPrefs(_ prefs: NightscoutPreferences) -> AnyPublisher<Void, Swift.Error> {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = Config.sharePath
+
+        var request = URLRequest(url: components.url!)
+        request.allowsConstrainedNetworkAccess = false
+        request.timeoutInterval = Config.timeout
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        request.httpBody = try! JSONCoding.encoder.encode(prefs)
+        request.httpMethod = "POST"
+
+        return service.run(request)
+            .retry(Config.retryCount)
+            .map { _ in () }
+            .eraseToAnyPublisher()
+    }
+
+    func uploadSettings(_ settings: NightscoutSettings) -> AnyPublisher<Void, Swift.Error> {
+        var components = URLComponents()
+        components.scheme = url.scheme
+        components.host = url.host
+        components.port = url.port
+        components.path = Config.sharePath
+
+        var request = URLRequest(url: components.url!)
+        request.allowsConstrainedNetworkAccess = false
+        request.timeoutInterval = Config.timeout
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        request.httpBody = try! JSONCoding.encoder.encode(settings)
+        request.httpMethod = "POST"
+
+        return service.run(request)
+            .retry(Config.retryCount)
+            .map { _ in () }
             .eraseToAnyPublisher()
     }
 }
